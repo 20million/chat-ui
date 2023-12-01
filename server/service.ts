9ee1f3643b0de87b539b03c31bcb1be7fb0ce402
shanks-20million <shanks.20million@gmail.com>
2023-12-01 11:55:04 +0530

    feat: redirect to https for tales.hellonancy.com

--- server/service.ts
+++ server/service.ts
@@ -1,6 +1,21 @@
+// Extend Request type to include protocol property
+declare module 'express' {
+    interface Request {
+        secure: boolean;
+        headers: any;
+        url: string;
+    }
+    interface Response {
+        redirect(url: string): void;
+        redirect(status: number, url: string): void;
+        redirect(url: string, status: number): void;
+        end(): any;
+    }
+}
+
 // server.ts
 import { handler } from '../build/handler.js';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import fs from 'fs';
 import http from 'http';
 import https from 'https';
@@ -10,6 +25,20 @@ const certificate = fs.readFileSync('./crt/fullchain.pem', 'utf8');
 const credentials = { key: privateKey, cert: certificate };
 
 const app = express();
+
+// Custom middleware for HTTP to HTTPS redirection
+const redirectHttpToHttps = (req: Request, res: Response, next: NextFunction) => {
+    const redirectTo = `https://${req.headers?.host}${req.url}`;
+    if (!req.secure) {
+        console.log(`Redirect HTTP to ${redirectTo}`);
+        res.redirect(301, redirectTo);
+    } else {
+        next();
+    }
+};
+
+// Apply middleware for HTTP to HTTPS redirection
+app.use(redirectHttpToHttps);
 
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
@@ -25,10 +54,10 @@ httpsServer.listen(SSLPORT, () => {
     console.log(`HTTPS Server is running on: https://localhost:${SSLPORT}`);
 });
 
-// add a route that lives separately from the SvelteKit app
-app.get('/healthcheck', (req, res) => {
-    res.end('ok');
+// Add a route that lives separately from the SvelteKit app
+app.get('/healthcheck', (req: Request, res: Response) => {
+    res.end();
 });
 
-// let SvelteKit handle everything else, including serving prerendered pages and static assets
+// Let SvelteKit handle everything else, including serving prerendered pages and static assets
 app.use(handler);
